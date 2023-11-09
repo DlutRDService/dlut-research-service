24ecc1ae40dead9baf6f7fe729d5bc16035e63b7
zsl <zsl2021@mail.dlut.edu.cn>
2023-11-09 14:18:28 +0800

    2023/11/09 配置Neo4j

--- src/main/java/com/dlut/ResearchService/service/impl/Neo4jServiceImpl.java
+++ src/main/java/com/dlut/ResearchService/service/impl/Neo4jServiceImpl.java
@@ -6,9 +6,13 @@ import com.dlut.ResearchService.entity.constants.StatusCode;
 import com.dlut.ResearchService.service.INeo4jService;
 import jakarta.annotation.Resource;
 import org.neo4j.driver.Driver;
+import org.neo4j.driver.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static org.neo4j.driver.Values.parameters;
+
 @Service
 public class Neo4jServiceImpl implements INeo4jService {
     @Resource
@@ -18,13 +22,22 @@ public class Neo4jServiceImpl implements INeo4jService {
     @Override
     public Result getCoAuthorIds(Integer author_id) {
         List<Integer> data = null;
-        return resultBuilder.build(StatusCode.STATUS_CODE_200, "Success", data);
+        return resultBuilder.build(StatusCode.STATUS_CODE_200, "Success", null);
     }
 
     @Override
     public Result queryRelatedGraph(Integer id) {
-        driver.getClass();
+        try (var session = driver.session()) {
+            var greeting = session.executeWrite(tx -> {
+                var query = new Query(
+                        "Match (n:Author) Where n.message = $message RETURN n.message + ', from node ' + id(n)",
+                        parameters("message", "2")
+                );
+                var result = tx.run(query);
+                return result.single().get(0).asString();
+            });
+            System.out.println(greeting);
+        }
         return null;
     }
-
 }
